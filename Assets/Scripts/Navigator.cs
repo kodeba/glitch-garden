c1d02cf73f60f76f74da19f06f3fc50513fa7f8e
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-04-10 12:40:01 +0700

    feat: implement navigator

---
+++ Assets/Scripts/Navigator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    void Awake()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SplashScreen()
+    {
+        StartCoroutine(GameStartCoroutine());
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("GameStart");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(GameOverCoroutine());
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator GameOverCoroutine()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameOver");
+    }
+
+    private IEnumerator GameStartCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        LoadStartMenu();
+    }
+
+}
