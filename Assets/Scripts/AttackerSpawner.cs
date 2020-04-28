ee3bf04089d1c3f6f2b0db1202d81f4b6a86d239
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-04-28 12:45:40 +0700

    feat: add spawner attacker

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] int minSpawnDelay = 1;
+
+    [SerializeField] int maxSpawnDelay = 5;
+
+    [SerializeField] Attacker attackerPrefab;
+
+    bool isSpawn = true;
+
+    IEnumerator Start()
+    {
+        while (isSpawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+}
