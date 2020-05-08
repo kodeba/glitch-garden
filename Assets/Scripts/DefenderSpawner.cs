0606548d59c24741466e14d00f5085bf358b23d0
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-05-08 12:31:52 +0700

    feat: spawn defender when click

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    private void OnMouseDown()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        Instantiate(defender, transform.position, Quaternion.identity);
+    }
+
+}
