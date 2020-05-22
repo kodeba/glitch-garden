d885de48012a9aeca89845e30ee06d7def85c82f
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-05-22 12:23:20 +0700

    feat: increase star with anim event for trophy

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 100;
+
+    public void AddStars(int amount)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+    }
+
 }
