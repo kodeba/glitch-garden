8ddc77d359bab85e050b38169340bde9c6efd355
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-06-10 13:07:17 +0700

    feat: add damage dealer script

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject target = collision.gameObject;
+        if (target.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(target);
+        }
+    }
+
+}
