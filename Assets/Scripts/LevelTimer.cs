5be939797ba1e8fedd68edc7142d97c63bf380eb
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-07-01 12:37:59 +0700

    feat: add level timer slider

---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+
+    [Tooltip("leve time in second")]
+    [SerializeField] int levelTime = 10;
+
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        if(Time.timeSinceLevelLoad >= levelTime)
+        {
+            Debug.Log("Time up");
+        }
+    }
+}
